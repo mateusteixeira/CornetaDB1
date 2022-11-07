87912fa136c29b69ef8e41f84d2082474216662e
mateusteixeira <mateusteixeira_@hotmail.com>
2022-11-07 20:45:18 -0300

    Update WebSecurityConfig.java

--- Corneta-DB1/src/main/java/br/com/corneta/configuration/WebSecurityConfig.java
+++ Corneta-DB1/src/main/java/br/com/corneta/configuration/WebSecurityConfig.java
@@ -58,6 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/actuator/**").permitAll()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .antMatchers(HttpMethod.POST,"/user/").permitAll()
